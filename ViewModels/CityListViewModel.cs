a7ea3d545d1394005fc4312f3a0c5c30df9e42f7
WozniakAA <97339003+WozniakAA@users.noreply.github.com>
2022-01-13 23:40:03 +0500

    Add files via upload

---
+++ ViewModels/CityListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Delivery.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delivery.ViewModels
+{
+    public class CityListViewModel
+    {
+        public IEnumerable<ModelCity> GetAllCities { get; set; }
+
+        public string DelCateg { get; set; }
+    }
+}
